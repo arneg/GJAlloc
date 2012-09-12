e63150ed27220162bf5cbb214b5a046109485fb5
Arne Goedeke <el@laramies.com>
2012-09-12 20:40:30 +0200

    .

--- perf/test_gjnofree.h
+++ perf/test_gjnofree.h
@@ -2,6 +2,7 @@
 #define TEST_INIT(t)	static struct block_allocator allocator = BA_INIT(sizeof(struct t), 512)
 #define DYNAMIC_INIT(t)
 #define TEST_ALLOC(t)	ba_alloc(&allocator);
+#define TEST_FREE(t, p)	
 #define TEST_FREEALL(t) ba_free_all(&allocator);
 #ifdef BA_STATS
 # define TEST_DEINIT(t)	do { ba_print_stats(&allocator); ba_destroy(&allocator); } while (0)
--- perf/test_gjtemporary.h
+++ perf/test_gjtemporary.h
@@ -2,6 +2,7 @@
 #define TEST_INIT(t)	static struct ba_temporary allocator;
 #define DYNAMIC_INIT(t) ba_init_temporary(&allocator, sizeof(struct t), 512);
 #define TEST_ALLOC(t)	ba_talloc(&allocator);
+#define TEST_FREE(t, p)	
 #define TEST_FREEALL(t)	ba_tdestroy(&allocator);
 #define TEST_DEINIT(t)	do { } while (0)
 #define TEST_NUM_PAGES(t, v)	do { v = allocator.num_pages; } while(0)
